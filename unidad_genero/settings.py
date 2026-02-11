ab7e9810f7cdcfdeccf5ce017e2a067421becb02
ridelyEX <arturovalruiz@gmail.com>
2026-02-11 14:49:25 -0600

    descarga disponible de documentos en Documentos

--- unidad_genero/settings.py
+++ unidad_genero/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = BASE_DIR / 'documentos'
+MEDIA_URL = '/documentos/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
